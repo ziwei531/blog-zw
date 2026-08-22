5a8edf1fe35c8e9e7fff6fe0144f1f0377fc499a
ziwei531 <ziwei531@users.noreply.github.com>
2026-08-23 00:33:19 +0800

    refactor(game-corner): centralize simulator links

---
+++ _data/simulators.js
@@ -0,0 +1,12 @@
+const probabilitySimUrl = "https://ziwei531.github.io/probability-sim/index.html";
+
+export default {
+	coinFlip: {
+		embedUrl: `${ probabilitySimUrl }?embed=1`,
+		fullUrl : probabilitySimUrl,
+	},
+	gacha: {
+		embedUrl: `${ probabilitySimUrl }?view=gacha&embed=1`,
+		fullUrl : `${ probabilitySimUrl }?view=gacha`,
+	},
+};
